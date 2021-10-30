eaeccbf7c90d2592b08dfe53a5b7d514d8480a76
MaugouMio <maugoumine@gmail.com>
2021-10-31 00:59:47 +0800

    BUG修復與調整

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 mcdp make ../immortal soul datapack
-rd /S /Q "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\TESTTTTTTTTTTT\datapacks\immortal soul datapack"
-xcopy "C:\Users\MaugouMio\Desktop\immortal soul datapack\build\immortal soul datapack" "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\TESTTTTTTTTTTT\datapacks\immortal soul datapack" /E /Y /I /Q
+rd /S /Q "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\Immortal Soul test1\datapacks\immortal soul datapack"
+xcopy "C:\Users\MaugouMio\Desktop\immortal soul datapack\build\immortal soul datapack" "C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\Immortal Soul test1\datapacks\immortal soul datapack" /E /Y /I /Q
 rd /S /Q "C:\Users\MaugouMio\Desktop\immortal soul datapack\build\immortal soul datapack"
 pause
