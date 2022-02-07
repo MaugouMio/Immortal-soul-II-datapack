221412e545a0e0abf4b591f44e195ed5ea12fbfd
MaugouMio <maugoumine@gmail.com>
2022-02-07 23:45:35 +0800

    animated java輸出設定

---
+++ AFTER_AJ_OUTPUT.bat
@@ -0,0 +1,6 @@
+REM animated java輸出資料包後手動增加 tag @s add effect
+
+ECHO. >>"C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\Immortal Soul test1\datapacks\immortal soul datapack\data\miko\functions\summon\zzz\execute\4.mcfunction"
+ECHO tag @s add effect>>"C:\Users\MaugouMio\AppData\Roaming\.minecraft\saves\Immortal Soul test1\datapacks\immortal soul datapack\data\miko\functions\summon\zzz\execute\4.mcfunction"
+
+pause
